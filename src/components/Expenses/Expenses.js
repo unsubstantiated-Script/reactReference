79692f47f542160f6585ed78acb1574a3ce6113c
Justin Fulton <jusfulton@gmail.com>
2021-08-06 17:39:55 -0700

    filter item added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "../UI/NewExpense/ExpensesFilter";
 
 const Expenses = ({ expenses }) => {
+	const [pickedYear, setPickedYear] = useState("2020");
+
+	console.log(pickedYear);
 	return (
-		<Card className='expenses'>
-			{expenses.map((expense) => (
-				<ExpenseItem key={expense.id} expense={expense} />
-			))}
-		</Card>
+		<>
+			<Card className='expenses'>
+				<ExpensesFilter
+					selectYearState={setPickedYear}
+					yearState={pickedYear}
+				/>
+				{expenses.map((expense) => (
+					<ExpenseItem key={expense.id} expense={expense} />
+				))}
+			</Card>
+		</>
 	);
 };
 
